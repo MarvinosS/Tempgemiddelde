42f6be991c2e6de8cb189cad8c4a2c61d50e0f00
marvi <marvi@MSI>
2021-09-21 12:26:21 +0200

    Add project files.

---
+++ Tempgemiddelde/Form1.Designer.cs
@@ -0,0 +1,426 @@
+﻿
+namespace Tempgemiddelde
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.frmTemp = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAfwZo = new System.Windows.Forms.TextBox();
+            this.txtAfwMa = new System.Windows.Forms.TextBox();
+            this.txtAfwDi = new System.Windows.Forms.TextBox();
+            this.txtAfwVr = new System.Windows.Forms.TextBox();
+            this.txtAfwDo = new System.Windows.Forms.TextBox();
+            this.txtAfwWo = new System.Windows.Forms.TextBox();
+            this.txtAfwZa = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtZa = new System.Windows.Forms.TextBox();
+            this.txtVr = new System.Windows.Forms.TextBox();
+            this.txtDo = new System.Windows.Forms.TextBox();
+            this.txtWo = new System.Windows.Forms.TextBox();
+            this.txtDi = new System.Windows.Forms.TextBox();
+            this.txtMo = new System.Windows.Forms.TextBox();
+            this.txtZo = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.btnBereken = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // frmTemp
+            // 
+            this.frmTemp.AutoSize = true;
+            this.frmTemp.Location = new System.Drawing.Point(47, 20);
+            this.frmTemp.Name = "frmTemp";
+            this.frmTemp.Size = new System.Drawing.Size(282, 15);
+            this.frmTemp.TabIndex = 0;
+            this.frmTemp.Text = "De gemiddelde dagtemperatuur over 1 jaar is 10.2 C.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(404, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Voer voor elke dag van de week de temperatuur in, afgerond op 1 decimaal.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Invoer temp per dag.";
+            // 
+            // txtAfwZo
+            // 
+            this.txtAfwZo.Location = new System.Drawing.Point(636, 130);
+            this.txtAfwZo.Name = "txtAfwZo";
+            this.txtAfwZo.ReadOnly = true;
+            this.txtAfwZo.Size = new System.Drawing.Size(100, 23);
+            this.txtAfwZo.TabIndex = 17;
+            // 
+            // txtAfwMa
+            // 
+            this.txtAfwMa.Location = new System.Drawing.Point(636, 172);
+            this.txtAfwMa.Name = "txtAfwMa";
+            this.txtAfwMa.ReadOnly = true;
+            this.txtAfwMa.Size = new System.Drawing.Size(100, 23);
+            this.txtAfwMa.TabIndex = 18;
+            // 
+            // txtAfwDi
+            // 
+            this.txtAfwDi.Location = new System.Drawing.Point(636, 207);
+            this.txtAfwDi.Name = "txtAfwDi";
+            this.txtAfwDi.ReadOnly = true;
+            this.txtAfwDi.Size = new System.Drawing.Size(100, 23);
+            this.txtAfwDi.TabIndex = 19;
+            // 
+            // txtAfwVr
+            // 
+            this.txtAfwVr.Location = new System.Drawing.Point(636, 326);
+            this.txtAfwVr.Name = "txtAfwVr";
+            this.txtAfwVr.ReadOnly = true;
+            this.txtAfwVr.Size = new System.Drawing.Size(100, 23);
+            this.txtAfwVr.TabIndex = 22;
+            // 
+            // txtAfwDo
+            // 
+            this.txtAfwDo.Location = new System.Drawing.Point(636, 289);
+            this.txtAfwDo.Name = "txtAfwDo";
+            this.txtAfwDo.ReadOnly = true;
+            this.txtAfwDo.Size = new System.Drawing.Size(100, 23);
+            this.txtAfwDo.TabIndex = 21;
+            // 
+            // txtAfwWo
+            // 
+            this.txtAfwWo.Location = new System.Drawing.Point(636, 241);
+            this.txtAfwWo.Name = "txtAfwWo";
+            this.txtAfwWo.ReadOnly = true;
+            this.txtAfwWo.Size = new System.Drawing.Size(100, 23);
+            this.txtAfwWo.TabIndex = 20;
+            // 
+            // txtAfwZa
+            // 
+            this.txtAfwZa.Location = new System.Drawing.Point(636, 370);
+            this.txtAfwZa.Name = "txtAfwZa";
+            this.txtAfwZa.ReadOnly = true;
+            this.txtAfwZa.Size = new System.Drawing.Size(100, 23);
+            this.txtAfwZa.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(532, 370);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(57, 15);
+            this.label11.TabIndex = 37;
+            this.label11.Text = "Zaterdag:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(532, 326);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 15);
+            this.label12.TabIndex = 36;
+            this.label12.Text = "Vrijdag:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(532, 289);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(69, 15);
+            this.label13.TabIndex = 35;
+            this.label13.Text = "Donderdag:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(532, 241);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(66, 15);
+            this.label14.TabIndex = 34;
+            this.label14.Text = "Woensdag:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(532, 207);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(53, 15);
+            this.label15.TabIndex = 33;
+            this.label15.Text = "Dinsdag:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(532, 172);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(60, 15);
+            this.label16.TabIndex = 32;
+            this.label16.Text = "Maandag:";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(532, 130);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(51, 15);
+            this.label17.TabIndex = 31;
+            this.label17.Text = "Zondag:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "Zaterdag:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 329);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 15);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Vrijdag:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 15);
+            this.label6.TabIndex = 49;
+            this.label6.Text = "Donderdag:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 244);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 15);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "Woensdag:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(47, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 15);
+            this.label8.TabIndex = 47;
+            this.label8.Text = "Dinsdag:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(47, 175);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 15);
+            this.label9.TabIndex = 46;
+            this.label9.Text = "Maandag:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(47, 133);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 15);
+            this.label10.TabIndex = 45;
+            this.label10.Text = "Zondag:";
+            // 
+            // txtZa
+            // 
+            this.txtZa.Location = new System.Drawing.Point(151, 373);
+            this.txtZa.Name = "txtZa";
+            this.txtZa.Size = new System.Drawing.Size(100, 23);
+            this.txtZa.TabIndex = 6;
+            // 
+            // txtVr
+            // 
+            this.txtVr.Location = new System.Drawing.Point(151, 329);
+            this.txtVr.Name = "txtVr";
+            this.txtVr.Size = new System.Drawing.Size(100, 23);
+            this.txtVr.TabIndex = 5;
+            // 
+            // txtDo
+            // 
+            this.txtDo.Location = new System.Drawing.Point(151, 292);
+            this.txtDo.Name = "txtDo";
+            this.txtDo.Size = new System.Drawing.Size(100, 23);
+            this.txtDo.TabIndex = 4;
+            // 
+            // txtWo
+            // 
+            this.txtWo.Location = new System.Drawing.Point(151, 244);
+            this.txtWo.Name = "txtWo";
+            this.txtWo.Size = new System.Drawing.Size(100, 23);
+            this.txtWo.TabIndex = 3;
+            // 
+            // txtDi
+            // 
+            this.txtDi.Location = new System.Drawing.Point(151, 210);
+            this.txtDi.Name = "txtDi";
+            this.txtDi.Size = new System.Drawing.Size(100, 23);
+            this.txtDi.TabIndex = 2;
+            // 
+            // txtMo
+            // 
+            this.txtMo.Location = new System.Drawing.Point(151, 175);
+            this.txtMo.Name = "txtMo";
+            this.txtMo.Size = new System.Drawing.Size(100, 23);
+            this.txtMo.TabIndex = 1;
+            // 
+            // txtZo
+            // 
+            this.txtZo.Location = new System.Drawing.Point(151, 133);
+            this.txtZo.Name = "txtZo";
+            this.txtZo.Size = new System.Drawing.Size(100, 23);
+            this.txtZo.TabIndex = 0;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(532, 98);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(90, 15);
+            this.label18.TabIndex = 52;
+            this.label18.Text = "Temp afwijking.";
+            // 
+            // btnBereken
+            // 
+            this.btnBereken.Location = new System.Drawing.Point(179, 411);
+            this.btnBereken.Name = "btnBereken";
+            this.btnBereken.Size = new System.Drawing.Size(443, 27);
+            this.btnBereken.TabIndex = 53;
+            this.btnBereken.Text = "Bereken de temperatuur afwijking voor elke dag op het jaargemiddelde";
+            this.btnBereken.UseVisualStyleBackColor = true;
+            this.btnBereken.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBereken);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtZa);
+            this.Controls.Add(this.txtVr);
+            this.Controls.Add(this.txtDo);
+            this.Controls.Add(this.txtWo);
+            this.Controls.Add(this.txtDi);
+            this.Controls.Add(this.txtMo);
+            this.Controls.Add(this.txtZo);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.txtAfwZa);
+            this.Controls.Add(this.txtAfwVr);
+            this.Controls.Add(this.txtAfwDo);
+            this.Controls.Add(this.txtAfwWo);
+            this.Controls.Add(this.txtAfwDi);
+            this.Controls.Add(this.txtAfwMa);
+            this.Controls.Add(this.txtAfwZo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.frmTemp);
+            this.Name = "Form1";
+            this.Text = "TemperatuurAfwijking.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label frmTemp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAfwZo;
+        private System.Windows.Forms.TextBox txtAfwMa;
+        private System.Windows.Forms.TextBox txtAfwDi;
+        private System.Windows.Forms.TextBox txtAfwVr;
+        private System.Windows.Forms.TextBox txtAfwDo;
+        private System.Windows.Forms.TextBox txtAfwWo;
+        private System.Windows.Forms.TextBox txtAfwZa;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtZa;
+        private System.Windows.Forms.TextBox txtVr;
+        private System.Windows.Forms.TextBox txtDo;
+        private System.Windows.Forms.TextBox txtWo;
+        private System.Windows.Forms.TextBox txtDi;
+        private System.Windows.Forms.TextBox txtMo;
+        private System.Windows.Forms.TextBox txtZo;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Button btnBereken;
+    }
+}
+
---
+++ Tempgemiddelde/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tempgemiddelde
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double gemiDdelde = 10.2 ;
+
+            string[] 
+            txtAfwZo.Text = txtZo.Text;
+            txtAfwZa.Text = txtZa.Text;
+            txtAfwVr.Text = txtVr.Text;
+            txtAfwDo.Text = txtDo.Text;
+            txtAfwWo.Text = txtWo.Text;
+            txtAfwDi.Text = txtDi.Text;
+            txtAfwMa.Text = txtMo.Text;
+        }
+    }
+}
